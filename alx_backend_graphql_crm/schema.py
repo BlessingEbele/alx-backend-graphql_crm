62401e60b7bde2f4f254aedb9695904894b13855
blessingebele <blessmart.com@gmail.com>
2025-08-17 19:36:01 +0100

    schema_mutation.py

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -16,3 +16,20 @@ class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
+
+# alx_backend_graphql_crm/schema.py
+
+import graphene
+from crm.schema import Query as CRMQuery
+from crm.schema_mutation import Mutation as CRMMutation
+
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
